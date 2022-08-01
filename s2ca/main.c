ebf0f736fa819b0d8eec5b1cfce4a8e399862e53
cschy <631046883@qq.com>
2022-08-02 01:50:33 +0800

    不占用焦点，在vs中直接使用，todo::恢复以前剪切板数据

--- s2ca/main.c
+++ s2ca/main.c
@@ -1,16 +1,40 @@
-#include <stdio.h>
-#include <locale.h>
+#include <Windows.h>
+#pragma comment(linker, "/subsystem:\"windows\" /entry:\"wmainCRTStartup\"")
 
 int wmain(int argc, wchar_t* argv[])
 {
-	setlocale(LC_ALL, "chs");
-	wchar_t *p = argv[1];
-	if (p != NULL) {
-		printf("{");
-		while (*p) {
-			wprintf_s(L"L'%c',", *p++);
-		}
-		printf("0}");
+	wchar_t* arg = argv[1];
+	if (arg == NULL) {
+		return;
 	}
+	const size_t size = (wcslen(arg) + 1) * 5 * sizeof(wchar_t);
+
+	if (OpenClipboard(NULL))
+	{
+		EmptyClipboard();
+		HGLOBAL buffer = GlobalAlloc(GMEM_FIXED, size);
+		wchar_t* p = (wchar_t*)GlobalLock(buffer);
+		*p++ = L'{';
+		while(*arg) {
+			*p++ = L'L'; 
+			*p++ = L'\'';
+			*p++ = *arg;
+			*p++ = L'\'';
+			*p++ = L',';
+			arg++;
+		}
+		*p++ = L'0';
+		*p++ = L'}';
+		*p = 0;
+		GlobalUnlock(buffer);
+		SetClipboardData(CF_UNICODETEXT, buffer);
+		CloseClipboard();
+	}
+
+	keybd_event(0x11, 0, 0, 0);//press ctrl
+	keybd_event(0x56, 0, 0, 0);//press v
+	keybd_event(0x56, 0, 2, 0);//release v
+	keybd_event(0x11, 0, 2, 0);//release ctrl
+	
 	return 0;
 }

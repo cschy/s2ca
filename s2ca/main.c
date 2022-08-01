755c5f0062adc57489dbe238e15666400da75d4c
cschy <631046883@qq.com>
2022-08-01 23:11:27 +0800

    优化

--- s2ca/main.c
+++ s2ca/main.c
@@ -6,15 +6,11 @@ int wmain(int argc, wchar_t* argv[])
 	setlocale(LC_ALL, "chs");
 	wchar_t *p = argv[1];
 	if (p != NULL) {
-		while (1) {
-			wprintf_s(L"L'%c'", *p);
-			if (*++p != 0) {
-				printf(",");
-			}
-			else {
-				break;
-			}
+		printf("{");
+		while (*p) {
+			wprintf_s(L"L'%c',", *p++);
 		}
+		printf("0}");
 	}
 	return 0;
 }
